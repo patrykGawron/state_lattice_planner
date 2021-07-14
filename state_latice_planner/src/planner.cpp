aa803964a6705de097bc98598457fed1140bb449
Patryk Gawron <patryk.gawron@outlook.com>
2021-07-14 19:41:10 +0200

    Basic path arc publisher

--- state_latice_planner/src/planner.cpp
+++ state_latice_planner/src/planner.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <ros/ros.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
 
 enum Orientation 
 {
@@ -50,14 +52,57 @@ struct State
     Orientation orientation = N;
 };
 
+double deg2rad(double deg)
+{
+    return (deg/180.0) * M_PI;
+}
+
+double rad2deg(double rad)
+{
+    return (rad/M_PI) * 180.0;
+}
+
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "planner");
     ros::NodeHandle handle;
-    Orientation orie = N;
 
+    nav_msgs::Path path;
+    path.header.stamp = ros::Time::now();
+    path.header.frame_id = "map";
+    double r = 1;
+    double start_x = 0;
+    double start_y = 0;
+    double end_x = 1;
+    double end_y = 1;
+    double steps = 100;
+    double increment = abs(end_x - start_x) / steps;
+    for(double angle = 0.0; angle < 90; ++angle)
+    {
+        double center_x = end_x;
+        double center_y = start_y;
+        geometry_msgs::PoseStamped pose;
+        pose.header.stamp = ros::Time::now();
+        pose.header.frame_id = "map";
+        pose.header.seq = 0;
+        pose.pose.position.x = center_x - cos(deg2rad(angle));
+        pose.pose.position.y = sin(deg2rad(angle));
+        pose.pose.position.z = 0;
+        pose.pose.orientation.w = 1;
+        pose.pose.orientation.x = 0;
+        pose.pose.orientation.y = 0;
+        pose.pose.orientation.z = 0;
+        path.poses.push_back(pose);
+    }
+
+    ros::Publisher path_publisher = handle.advertise<nav_msgs::Path>("path", 1000);
+    int a = 0;
+    for(int x = 0; x < 1000000; x++)
+    {
+        path_publisher.publish(path);
+    }
     std::cout << "Hello World\n";
-    std::cout << --orie << "\n";
+    std::cout << deg2rad(90.0) << "\n";
     return 0;
 }
