8d9a1e94c12aad9a6d0ccc4ec43fa99dda42da43
Patryk Gawron <patryk.gawron@outlook.com>
2021-08-01 16:26:43 +0200

    Added movement primitives and launch file

--- state_latice_planner/src/planner.cpp
+++ state_latice_planner/src/planner.cpp
@@ -50,6 +50,13 @@ struct State
 {
     int x = 0, y = 0;
     Orientation orientation = N;
+
+    float gCost, hCost;
+
+    bool operator==(State& other)
+    {
+        return x == other.x && y == other.y && orientation == other.orientation;
+    }
 };
 
 double deg2rad(double deg)
@@ -62,32 +69,28 @@ double rad2deg(double rad)
     return (rad/M_PI) * 180.0;
 }
 
-
-int main(int argc, char **argv)
+struct Point2D
 {
-    ros::init(argc, argv, "planner");
-    ros::NodeHandle handle;
+    double x, y;
+};
 
+nav_msgs::Path draw_cubic_bezier(Point2D p1, Point2D p2, Point2D p3, Point2D p4)
+{
     nav_msgs::Path path;
     path.header.stamp = ros::Time::now();
     path.header.frame_id = "map";
-    double r = 1;
-    double start_x = 0;
-    double start_y = 0;
-    double end_x = 1;
-    double end_y = 1;
-    double steps = 100;
-    double increment = abs(end_x - start_x) / steps;
-    for(double angle = 0.0; angle < 90; ++angle)
+    double dt = 0.01;
+    for(double t = 0; t <= 1; t += dt)
     {
-        double center_x = end_x;
-        double center_y = start_y;
         geometry_msgs::PoseStamped pose;
         pose.header.stamp = ros::Time::now();
         pose.header.frame_id = "map";
         pose.header.seq = 0;
-        pose.pose.position.x = center_x - cos(deg2rad(angle));
-        pose.pose.position.y = sin(deg2rad(angle));
+
+        // Third order Bezier Curve
+        pose.pose.position.x = p1.x * (1 - t) * (1 - t) * (1 - t) + 3 * p2.x * t * (1 - t) * (1 - t) + 3 * p3.x * t * t * (1 - t) + p4.x * t * t * t;
+        pose.pose.position.y = p1.y * (1 - t) * (1 - t) * (1 - t) + 3 * p2.y * t * (1 - t) * (1 - t) + 3 * p3.y * t * t * (1 - t) + p4.y * t * t * t;
+
         pose.pose.position.z = 0;
         pose.pose.orientation.w = 1;
         pose.pose.orientation.x = 0;
@@ -95,14 +98,43 @@ int main(int argc, char **argv)
         pose.pose.orientation.z = 0;
         path.poses.push_back(pose);
     }
+    return path;
+}
 
-    ros::Publisher path_publisher = handle.advertise<nav_msgs::Path>("path", 1000);
-    int a = 0;
-    for(int x = 0; x < 1000000; x++)
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "planner");
+    ros::NodeHandle handle;
+
+    std::cout << "State Lattice Planner started!\n";
+
+    std::vector<std::string> names = {"forward_right", "forward_left", "forward", "backward_right", "backward_left", "backward"};
+
+    std::vector<ros::Publisher> publishers;
+    publishers.reserve(6);
+
+    for(int i = 0; i < 6; i++)
     {
-        path_publisher.publish(path);
+        publishers.push_back(handle.advertise<nav_msgs::Path>(names[i], 1000));
     }
-    std::cout << "Hello World\n";
-    std::cout << deg2rad(90.0) << "\n";
+
+    nav_msgs::Path forward_right = draw_cubic_bezier({0, 0}, {0, 1}, {0.75, 0.75}, {1, 1});
+    nav_msgs::Path forward_left = draw_cubic_bezier({0, 0}, {0, 1}, {-0.75, 0.75}, {-1, 1});
+    nav_msgs::Path forward = draw_cubic_bezier({0, 0}, {0, 1}, {0, 0.5}, {0, 1});
+    nav_msgs::Path backward_right = draw_cubic_bezier({0, 0}, {0, -1}, {0.75, -0.75}, {1, -1});
+    nav_msgs::Path backward_left = draw_cubic_bezier({0, 0}, {0, -1}, {-0.75, -0.75}, {-1, -1});
+    nav_msgs::Path backward = draw_cubic_bezier({0, 0}, {0, -1}, {0, -0.5}, {0, -1});
+
+    while(ros::ok())
+    {
+        publishers[0].publish(forward_right);
+        publishers[1].publish(forward_left);
+        publishers[2].publish(forward);
+        publishers[3].publish(backward_right);
+        publishers[4].publish(backward_left);
+        publishers[5].publish(backward);
+    }
+    std::cout << "Planner has finished!\n";
     return 0;
 }
